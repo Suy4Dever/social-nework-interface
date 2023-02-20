c84e4a0b44f72731a036f4212356d737604b3a46
Suy4Dever <khatbde180162@fpt.edu.vn>
2023-02-20 18:45:10 +0700

    fix effect click

--- scripts.js
+++ scripts.js
@@ -2,9 +2,14 @@ let counter1 = 0;
 document.getElementsByClassName("info-1")[0].addEventListener("click", () => {
   if (counter1 === 0) {
     document.getElementsByClassName("detail-1")[0].style.display = "block";
+    document.getElementsByClassName("title-1")[0].style.backgroundColor =
+      "#57707d";
+    document.getElementsByClassName("title-1")[0];
     counter1 = 1;
   } else {
     document.getElementsByClassName("detail-1")[0].style.display = "none";
+    document.getElementsByClassName("title-1")[0].style.backgroundColor =
+      "#7d97a5";
     counter1 = 0;
   }
 });
@@ -13,9 +18,13 @@ let counter2 = 0;
 document.getElementsByClassName("info-2")[0].addEventListener("click", () => {
   if (counter2 === 0) {
     document.getElementsByClassName("detail-2")[0].style.display = "block";
+    document.getElementsByClassName("title-2")[0].style.backgroundColor =
+      "#57707d";
     counter2 = 1;
   } else {
     document.getElementsByClassName("detail-2")[0].style.display = "none";
+    document.getElementsByClassName("title-2")[0].style.backgroundColor =
+      "#7d97a5";
     counter2 = 0;
   }
 });
